9ba68599c987268895d0e8e9cc2f9e0fae19df83
sefaunal <83312431+sefaunal@users.noreply.github.com>
2024-12-25 14:03:06 +0300

    Initial Commit

---
+++ src/main/java/com/sefaunal/authguard/AuthguardApplication.java
@@ -0,0 +1,13 @@
+package com.sefaunal.authguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthguardApplication.class, args);
+	}
+
+}
